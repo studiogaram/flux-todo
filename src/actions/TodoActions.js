60ce564103b407fa881c74b7b7ddeb8bb038986d
garam <studio@gar.am>
2016-04-23 17:47:14 +0900

    Add ToDo Actions. Edit name of constants in TodoCanstants.js

---
+++ src/actions/TodoActions.js
@@ -0,0 +1,15 @@
+var AppDispatcher = require('../dispatcher/AppDispatcher');
+var TodoConstants = require('../constants/TodoConstants');
+
+var TodoActions = {
+
+    create: function(text) {
+        AppDispatcher.dispatch({
+            actionType: TodoConstants.TODO_CREATE,
+            text: text
+        });
+    },
+
+};
+
+module.exports = TodoActions;
